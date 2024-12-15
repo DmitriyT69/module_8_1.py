6f2fb1c5491726cb7e5250acb7c7c0cb9596ccbd
DmitriyT69 <td2598793@gmail.com>
2024-12-15 17:45:33 +0400

    module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -9,4 +9,4 @@ def add_everything_up(a, b):
 if __name__ == '__main__':
     print(add_everything_up(123.456, 'строка'))
     print(add_everything_up('яблоко', 4215))
-    print(add_everything_up(123.456, 
+    print(add_everything_up(123.456, 7))
