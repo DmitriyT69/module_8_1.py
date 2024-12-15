c334b117505bfcc56c6a7af8b98b3ae0fcbb7f47
DmitriyT69 <td2598793@gmail.com>
2024-12-15 17:44:21 +0400

    module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,10 +1,12 @@
 def add_everything_up(a, b):
     try:
-        result = round(a + b, 3)
+        sum = a + b
     except TypeError:
-        result = str(a) + str(b)
-    return result
+        sum = str(a) + str(b)
+    return sum
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+
+if __name__ == '__main__':
+    print(add_everything_up(123.456, 'строка'))
+    print(add_everything_up('яблоко', 4215))
+    print(add_everything_up(123.456, 
